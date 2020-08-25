dc5b1cde09fc60e7c1abf6f176ccfae36bc76e3a
andre.machado <4ndre.machado@gmail.com>
2020-08-25 12:47:24 -0300

    Showing situations according to BMI results

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BMI Calculator
 
 Calculator of Body Mass Index
+
+![alt text](https://github.com/alm9/body_mass_index_calc/blob/master/figures/BMI_screen.png)
--- lib/bmi.dart
+++ lib/bmi.dart
@@ -29,20 +29,28 @@ class _BmiState extends State<Bmi> {
             content: Text('Type height and weight', style: TextStyle(fontSize: 20),),
           ));
         }else{
-        try {
-          var weight = double.parse(_weightController.text);
-          var height = double.parse(_heightController.text);
-          var bmi = weight / (height * height);
-          //          debugPrint(bmi.toStringAsFixed(2)); //print 2 decimal places
-          setState(() {
-            _result = 'BMI = ${bmi.toStringAsFixed(2)}';
-          });
-        } catch (e) {
-          key.currentState.showSnackBar(SnackBar(
-          content: Text('Invalid height or weight', style: TextStyle(fontSize: 20),),
-        ));
+          try {
+            var weight = double.parse(_weightController.text);
+            var height = double.parse(_heightController.text);
+            var bmi = weight / (height * height);
+            //          debugPrint(bmi.toStringAsFixed(2)); //print 2 decimal places
+            setState(() {
+              _result = 'BMI = ${bmi.toStringAsFixed(2)}';
+              //situations:
+              if (bmi<17) _situation = 'Very underweight';
+              else if (bmi<18.5) _situation = 'Underweight';
+              else if (bmi<25) _situation = 'Normal weight';
+              else if (bmi<30) _situation = 'Overweight';
+              else if (bmi<35) _situation = 'Obesity';
+              else if (bmi<40) _situation = 'Severe obesity';
+              else _situation = 'Morbid obesity';
+            });
+          } catch (e) {
+            key.currentState.showSnackBar(SnackBar(
+            content: Text('Invalid height or weight', style: TextStyle(fontSize: 20),),
+          ));
 
-        }
+          }
         }
     }
   }
@@ -81,6 +89,7 @@ class _BmiState extends State<Bmi> {
             ),
           ),
           Text('$_result', style: TextStyle(fontSize: 30)),
+          Text('$_situation', style: TextStyle(fontSize: 30)),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
